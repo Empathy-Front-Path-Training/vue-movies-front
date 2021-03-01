e781b5041abcd3f3eac920c20b5a71a991a576ca
JustDMare <UO264608@uniovi.es>
2021-03-01 14:23:38 +0100

    Separated the initial project into different files to test separation of concerns

--- src/components/HelloWorld/HelloWorld.vue
+++ src/components/HelloWorld/HelloWorld.vue
@@ -118,29 +118,7 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
-};
-</script>
+<script src="./script.js"></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style src="./style.scss" scoped lang="scss"></style>
