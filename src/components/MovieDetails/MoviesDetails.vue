6fb0d6504dc33abd1deeb5e373e6ccc863c61bfe
JustDMare <UO264608@uniovi.es>
2021-03-03 15:17:25 +0100

    Small hotfix for the details of the movies. Changed image border radius and size.

--- src/components/MovieDetails/MoviesDetails.vue
+++ src/components/MovieDetails/MoviesDetails.vue
@@ -1,14 +1,15 @@
 <template>
   <transition name="fade">
     <section v-show="movie.id !== undefined" id="movie-details">
-      <div>
+      <h2>Details of the movie</h2>
+      <section>
         <img alt="mock image for the poster" src="../../assets/stand-by.jpg" />
-      </div>
-      <h2>{{ movie.title }}</h2>
-      <h3>Rating: {{ movie.averageRating }}</h3>
-      <h3>Year of release: {{ movie.startYear }}</h3>
-      <h4>Type: {{ movie.type }}</h4>
-      <h4>Genres: {{ listGenres() }}</h4>
+        <h2>{{ movie.title }}</h2>
+        <h3>Rating: {{ movie.averageRating }}</h3>
+        <h3>Year of release: {{ movie.startYear }}</h3>
+        <h4>Type: {{ movie.type }}</h4>
+        <h4>Genres: {{ listGenres() }}</h4>
+      </section>
     </section>
   </transition>
 </template>
