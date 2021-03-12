005c79ba5acaf3363648beed84228e99b5060cab
JustDMare <UO264608@uniovi.es>
2021-03-12 13:55:51 +0100

    Finished the inclusion of the calls to the OMdb API for the poster. Also fixed some UI minor issues

--- src/components/MovieDetails/MoviesDetails.vue
+++ src/components/MovieDetails/MoviesDetails.vue
@@ -1,10 +1,10 @@
 <template>
   <transition name="fade">
     <section v-show="movieInfo.id !== undefined" id="movie-details-panel">
-      <h2>Details of the movie:</h2>
-      <img alt="mock image for the poster" :src="movieInfo.image" />
+      <h2 id="details-title">Details of the movie:</h2>
       <transition name="fade">
         <section v-show="movieInfo.id !== undefined" :key="movieInfo.id">
+          <img :src="moviePoster" alt="mock image for the poster" />
           <h2 class="movie-title">{{ movie.title }}</h2>
           <h3>Rating: {{ movie.averageRating }}</h3>
           <h3>Year of release: {{ movie.startYear }}</h3>
@@ -21,6 +21,7 @@
 </template>
 
 <script>
+// http://www.omdbapi.com/?i=[id]&apikey=a5f8e3c5
 export default {
   name: "MoviesDetails",
   props: {
@@ -31,15 +32,35 @@ export default {
   data() {
     return {
       movie: {},
+      moviePoster: "",
     };
   },
   methods: {
     showSelectedMovie(selectedMovieId) {
       if (selectedMovieId !== "") {
-        this.fetchSelectedMovie(selectedMovieId).then((movie) => {
-          this.movie = movie;
-        });
+        this.fetchSelectedMovie(selectedMovieId).then(
+          (movie) => (this.movie = movie)
+        );
+        this.fetchMoviePosterFromOMdb(selectedMovieId).then(
+          (poster) => (this.moviePoster = poster)
+        );
       }
+    },
+    async fetchMoviePosterFromOMdb(selectedMovieId) {
+      let poster = "";
+      try {
+        let response = await fetch(
+          "http://www.omdbapi.com/?i=" + selectedMovieId + "&apikey=a5f8e3c5"
+        );
+        let movieJson = await response.json();
+        poster = movieJson.Poster;
+      } catch (e) {
+        console.log(
+          "There has been an error and the poster could not be fetched"
+        );
+        poster = require("@/assets/stand-by.jpg");
+      }
+      return poster;
     },
     async fetchSelectedMovie(selectedMovieId) {
       try {
